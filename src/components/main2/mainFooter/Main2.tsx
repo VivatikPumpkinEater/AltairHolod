c046ceaf326bd9ebd16e8cbd433cae085cae5edd
SashaPolyanski <Sashapolyanski@gmail.com>
2022-09-17 23:43:20 +0300

    privet Ivak

---
+++ src/components/main2/mainFooter/Main2.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import s from './Main2.module.css'
+
+
+
+
+const Main2 = () => {
+    return (
+        <div className={s.wrapper}>
+            
+        </div>
+    );
+};
+
+export default Main2;
